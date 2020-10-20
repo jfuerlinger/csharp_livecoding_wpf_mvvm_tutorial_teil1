db8cd5a658c3e0677c4aeb89c57db76ff711a281
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-10-20 16:12:04 +0200

    Initial Commit

---
+++ src/ActReport.ViewModel/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ActReport.ViewModel
+{
+  public class RelayCommand : ICommand
+  {
+    private readonly Action<object> _execute;
+    private readonly Predicate<object> _canExecute;
+
+    public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+    {
+      _execute = execute;
+      _canExecute = canExecute;
+    }
+
+    /// <summary>
+    /// Ist das Kommando ausführbar?
+    /// </summary>
+    public bool CanExecute(object parameter) => _canExecute != null && _canExecute(parameter);
+
+    /// <summary>
+    /// Führe das Kommando durch
+    /// </summary>
+    public void Execute(object parameter) => _execute(parameter);
+
+    /// <summary>
+    /// Event zur Benachrichtigung, wenn CanExecute sich geändert hat
+    /// </summary>
+    public event EventHandler CanExecuteChanged
+    {
+      add => CommandManager.RequerySuggested += value;
+      remove => CommandManager.RequerySuggested -= value;
+    }
+  }
+}
+
+
+
+
